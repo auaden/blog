7f835ac3b68fa7d97877a1f4c37db4425705900c
auaden <wlau.2014@sis.smu.edu.sg>
2020-01-20 22:42:47 +0800

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-156518619-1`,
       },
     },
     `gatsby-plugin-feed`,
